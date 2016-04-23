9a0f6049c27f302394b46141eb6f84bfbf537e2a
Kevin <kchiu6230@bths.edu>
2016-04-23 18:55:56 +0000

    Used nunjucks as the templating engine

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var consolidate = require('consolidate');
+var nunjucks = require('nunjucks');
 
 // Local dependencies
 var router = require('./routes');
@@ -15,8 +17,12 @@ var app = express();
 // view engine setup
 // Set the location of our views directory
 app.set('views', path.join(__dirname, 'views'));
-// Set jade as the default view engine
-app.set('view engine', 'jade');
+// Confugure nunkucks engine so that it knows where our views are located
+consolidate.requires.nunjucks = nunjucks.configure('views');
+// Assign nunjucks engine for .html files
+app.engine('html', consolidate.nunjucks);
+// Set html as the default view engine
+app.set('view engine', 'html');
 /*
 
 Middlewares setup
