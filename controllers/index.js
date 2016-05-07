85e04ef4f51d284f764ae68cfaed5659634678a7
Kevin <kchiu6230@bths.edu>
2016-05-07 19:11:22 +0000

    updated index page

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,10 @@
 // Local dependencies
 var About = require('./about');
 var Home = require('./home');
-var Users = require('./users');
 var Search = require('./search');
 
 module.exports = {
     About: About, //second "About" follows the variable name
     Home: Home,
-    Users: Users,
     Search: Search
 };
